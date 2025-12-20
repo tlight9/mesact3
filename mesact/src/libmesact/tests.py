83542e0f79ad1212d59b66eb1c6d6be1a9df8ddc
John Thornton <dev@gnipsel.com>
2025-12-20 10:39:32 -0600

    work on name tests
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- mesact/src/libmesact/tests.py
+++ mesact/src/libmesact/tests.py
@@ -1,6 +1,8 @@
 
 from functools import partial
 
+from PyQt6.QtCore import QObject
+
 from libmesact import dialogs
 from libmesact import download
 
@@ -19,6 +21,13 @@ def connect(parent):
 	parent.arm64_deb_pb.clicked.connect(partial(download.download_deb, 'arm64', parent))
 	parent.bad_url_pb.clicked.connect(partial(download.download_deb, parent))
 	parent.download_firmware_pb.clicked.connect(partial(download.download_firmware, parent))
+
+	parent.test_axis_names_pb.clicked.connect(partial(test_axis_names, parent))
+	parent.test_pid_names_pb.clicked.connect(partial(test_pid_names, parent))
+	parent.test_homing_names_pb.clicked.connect(partial(test_homing_names, parent))
+	parent.test_analog_names_pb.clicked.connect(partial(test_analog_names, parent))
+	parent.test_input_names_pb.clicked.connect(partial(test_input_names, parent))
+	parent.test_output_names_pb.clicked.connect(partial(test_output_names, parent))
 
 def test_dialogs(parent):
 	name = parent.sender().objectName()
@@ -48,5 +57,61 @@ def test_dialogs(parent):
 			result = dialogs.msg_ok(parent, 'Test', 'Title')
 			print(result)
 
+def test_axis_names(parent):
+	'''
+	c0_axis_0 c0_axis_type_0 c0_scale_0 c0_min_limit_0 c0_max_limit_0 c0_max_vel_0
+	c0_max_vel_suffix_0 c0_max_accel_0 c0_max_vel_min_0 c0_max_vel_min_suffix_0
+	'''
+	axis_list = ['axis', 'axis_type', 'scale', 'min_limit', 'max_limit',
+	'max_vel', 'max_vel_suffix', 'max_accel', 'max_vel_min', 'max_vel_min_suffix']
+	for item in axis_list:
+		for i in range(3):
+			for j in range (6):
+				if not parent.findChild(QObject, f'c{i}_{item}_{j}'):
+					print(f'******* c{i}_{item}_{j} Not Found *******')
+		print(f'All of the {item} names were tested')
+
+def test_pid_names(parent):
+	'''
+	c0_p_0 c0_i_0 c0_d_0 c0_ff0_0 c0_ff1_0 c0_ff2_0
+	c0_deadband_0 c0_bias_0 c0_max_output_0 c0_max_error_0 c0_pid_default_0
+	c0_min_ferror_0 c0_max_ferror_0 c0_ferror_default_0
+	'''
+
+	pid_list = ['p', 'i', 'd', 'ff0', 'ff1', 'ff2', 'deadband', 'bias',
+	'max_output', 'max_error', 'pid_default', 'min_ferror', 'max_ferror', 'ferror_default']
+	for item in pid_list:
+		for i in range(3):
+			for j in range (6):
+				if not parent.findChild(QObject, f'c{i}_{item}_{j}'):
+					print(f'******* c{i}_{item}_{j} Not Found *******')
+		print(f'All of the {item} names were tested')
+
+def test_homing_names(parent):
+	print('test_homing_names')
+	'''
+	c0_home_0 c0_home_offset_0 c0_home_search_vel_0 c0_home_latch_vel_0
+	c0_home_final_vel_0 c0_home_sequence_0 c0_home_use_index_0
+	c0_home_switch_shared_0 c0_home_ignore_limits_0
+	'''
+
+	home_list = ['home', 'home_offset', 'home_search_vel', 'home_latch_vel',
+	'home_final_vel', 'home_sequence', 'home_use_index', 'home_switch_shared',
+	'home_ignore_limits']
+	for item in home_list:
+		for i in range(3):
+			for j in range (6):
+				if not parent.findChild(QObject, f'c{i}_{item}_{j}'):
+					print(f'******* c{i}_{item}_{j} Not Found *******')
+		print(f'All of the {item} names were tested')
+
+def test_analog_names(parent):
+	print('test_analog_names')
+
+def test_input_names(parent):
+	print('test_input_names')
+
+def test_output_names(parent):
+	print('test_output_names')
 
 
